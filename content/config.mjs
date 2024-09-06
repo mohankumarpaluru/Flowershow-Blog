490108cb3826c8d8abbdadcba650a2053ae39ea6
mohankumarpaluru <mohankumarpaluru@gmail.com>
2024-09-06 11:24:15 +0530

    Configs_Set

--- content/config.mjs
+++ content/config.mjs
@@ -2,4 +2,30 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+const config = {
+    title: "📚 Curiosity Chronicles",
+    description: "These are my published Obsidian notes on a range of topics that I find interesting or cool",
+    author: "Mohan",
+    domain: "https://notes.trixtertempdrive.eu.org/",
+    search: {
+      provider: "kbar",
+    },
+    showComments: true,
+    comments: {
+      provider: "giscus",
+      config: {
+        repo: "mohankumarpaluru/curiosity-chronicles",
+        repositoryId: "R_kgDOL9hH7A",
+        category: "Announcements",
+        categoryId: "DIC_kwDOL9hH7M4CiOPg",
+      },
+    },
+    // links to the pages you want to link to in the navbar and in the footer
+    navLinks: [
+      { href: "/home", name: "Home" },
+      { href: "/about", name: "About" },
+      { href: "/AI/AI", name: "AI" }, // Assuming AI.md is the main page for AI
+      { href: "/AWS/AWS", name: "AWS" }, // You might want to choose a key file or landing page
+      ]};
+  
+  export default config;
