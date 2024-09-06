15704f2cb516b5de952858d14e11853a880561ce
mohankumarpaluru <mohankumarpaluru@gmail.com>
2024-09-06 14:43:24 +0530

    Test blog

--- content/config.mjs
+++ content/config.mjs
@@ -6,11 +6,13 @@ const config = {
     title: "📚 Curiosity Chronicles",
     description: "These are my published Obsidian notes on a range of topics that I find interesting or cool",
     author: "Mohan",
-    domain: "https://notes.trixtertempdrive.eu.org/",
+    domain: "https://mohan.is-a.dev/",
     // search: {
     //   provider: "kbar",
     // },
     showComments: true,
+    showSidebar: true,
+    showLinkPreviews: true,    
     comments: {
       provider: "giscus",
       config: {
@@ -20,9 +22,10 @@ const config = {
         categoryId: "DIC_kwDOL9hH7M4CiOPg",
       },
     },
+    blogDir: "Development",
     // links to the pages you want to link to in the navbar and in the footer
     navLinks: [
-      { href: "/home", name: "Home" },
+      { href: "/", name: "Home" },
       { href: "/About", name: "About" },
       { href: "/AI/Fundamentals/AI", name: "AI" }, // Assuming AI.md is the main page for AI
       { href: "/AWS/AWS", name: "AWS" }, // You might want to choose a key file or landing page
